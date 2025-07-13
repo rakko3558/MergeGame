0f36e3072109034118a3e7b93509378658d4ec4e
rakko <leilin4528@gmail.com>
2025-07-13 15:22:55 +0800

    feat(save): complete Firebase save system for game progress1

--- Assets/Scenes/CameraDrag.cs
+++ Assets/Scenes/CameraDrag.cs
@@ -169,23 +169,25 @@ public class CameraDrag : MonoBehaviour
         if (touchStatus == 2)
         {
 
-            touchZero = Input.GetTouch(0);
-            touchOne = Input.GetTouch(1);
+            Touch touch1 = Input.GetTouch(0);
+            Touch touch2 = Input.GetTouch(1);
+            if (touch1.phase == TouchPhase.Began && touch2.phase == TouchPhase.Began)
+            {
+                lastDist = Vector2.Distance(touch1.position, touch2.position);
+            }
 
-            // 計算上一幀與這一幀的距離差
-            float prevMagnitude = (prevTouchZeroPos - prevTouchOnePos).magnitude;
-            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+            else if (touch1.phase == TouchPhase.Moved && touch2.phase == TouchPhase.Moved)
+            {
+                float newDist = Vector2.Distance(touch1.position, touch2.position);
+                touchDist = lastDist - newDist;
+                lastDist = newDist;
 
-            float difference = currentMagnitude - prevMagnitude;
+                // Your Code Here
+                cam.orthographicSize += touchDist * 0.01f;
+                cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, PhoneMinZoom, PhoneMaxZoom);
 
-            // 縮放處理
-            cam.orthographicSize -= difference * 0.01f;
-            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, PhoneMinZoom, PhoneMaxZoom);
-
-            // 更新上一幀位置
-            prevTouchZeroPos = touchZero.position;
-            prevTouchOnePos = touchOne.position;
-
+                //Camera.main.fieldOfView += touchDist * 0.1f;
+            }
             /*  touchZero = Input.GetTouch(0);
              touchOne = Input.GetTouch(1);
 
